de9bdb791dc7c74659363497927a95f70556cf4f
iPsychopomp <brianexdd@gmail.com>
2025-03-03 17:00:57 +0800

    Upload connect.php

---
+++ includes/connect.php
@@ -0,0 +1,22 @@
+<?php
+    $host = "localhost";
+    $database = "dbbaste";
+    $user = "root";
+    $password = "";
+    $dsn = "mysql:host={$host};dbname={$database};";
+
+    // echo $dsn;
+
+    try
+    {
+        $con = new PDO($dsn, $user, $password);
+        if ($con)
+        {
+            // echo "Successfully connected to database.";
+        }
+    }
+    catch (PDOException $th)
+    {
+        echo $th->getMessage();
+    }
+?>
